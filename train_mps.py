e33a07815510f0827c15417a79bf0a23bc19eed5
sunjesse <jessera@outlook.com>
2024-09-13 11:04:17 -0400

    got mps train working

--- train_mps.py
+++ train_mps.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,39 +11,19 @@ from torch.optim.lr_scheduler import StepLR
 import torch.distributed as dist
 import torch.multiprocessing as mp
 from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.utils.data.distributed import DistributedSampler
-from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
-from torch.distributed.fsdp.fully_sharded_data_parallel import (
-    CPUOffload,
-    BackwardPrefetch,
-)
-from torch.distributed.fsdp.wrap import (
-    size_based_auto_wrap_policy,
-    enable_wrap,
-    wrap,
-)
 
 from model.net import Net
-
-def setup(rank, world_size):
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
-
-    dist.init_process_group("nccl", rank=rank, world_size=world_size)
-
-def cleanup():
-    dist.destroy_process_group()
 
 _device = "mps" if torch.backends.mps.is_available() else "cpu"
 device = torch.device(_device)
 
-def train(args, model, rank, world_size, train_loader, optimizer, epoch, sampler=None):
+def train(args, model, train_loader, optimizer, epoch, sampler=None):
     model.train()
-    ddp_loss = torch.zeros(2).to(rank)
+    ddp_loss = torch.zeros(2)
     if sampler:
         sampler.set_epoch(epoch)
     for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(rank), target.to(rank)
+        data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
         loss = F.nll_loss(output, target, reduction='sum')
@@ -51,76 +32,54 @@ def train(args, model, rank, world_size, train_loader, optimizer, epoch, sampler
         ddp_loss[0] += loss.item()
         ddp_loss[1] += len(data)
 
-    dist.all_reduce(ddp_loss, op=dist.ReduceOp.SUM)
-    if rank == 0:
-        print('Train Epoch: {} \tLoss: {:.6f}'.format(epoch, ddp_loss[0] / ddp_loss[1]))
+    print('Train Epoch: {} \tLoss: {:.6f}'.format(epoch, ddp_loss[0] / ddp_loss[1]))
 
-def test(model, rank, world_size, test_loader):
+def test(model, test_loader):
     model.eval()
     correct = 0
-    ddp_loss = torch.zeros(3).to(rank)
+    ddp_loss = torch.zeros(3)
     with torch.no_grad():
         for data, target in test_loader:
-            data, target = data.to(rank), target.to(rank)
+            data, target = data.to(device), target.to(device)
             output = model(data)
             ddp_loss[0] += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             ddp_loss[1] += pred.eq(target.view_as(pred)).sum().item()
             ddp_loss[2] += len(data)
 
-    dist.all_reduce(ddp_loss, op=dist.ReduceOp.SUM)
+    test_loss = ddp_loss[0] / ddp_loss[2]
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, int(ddp_loss[1]), int(ddp_loss[2]),
+        100. * ddp_loss[1] / ddp_loss[2]))
 
-    if rank == 0:
-        test_loss = ddp_loss[0] / ddp_loss[2]
-        print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-            test_loss, int(ddp_loss[1]), int(ddp_loss[2]),
-            100. * ddp_loss[1] / ddp_loss[2]))
-
-def fsdp_main(rank, world_size, args):
-    setup(rank, world_size)
-
+def run(args): 
     transform=transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-    ])
+        transforms.Normalize((0.1307,), (0.3081,))])
 
-    dataset1 = datasets.MNIST('../data', train=True, download=True,
+    dataset1 = datasets.MNIST('data/', train=True, download=True,
                         transform=transform)
-    dataset2 = datasets.MNIST('../data', train=False,
+    dataset2 = datasets.MNIST('data/', train=False,
                         transform=transform)
 
-    sampler1 = DistributedSampler(dataset1, rank=rank, num_replicas=world_size, shuffle=True)
-    sampler2 = DistributedSampler(dataset2, rank=rank, num_replicas=world_size)
-
-    train_kwargs = {'batch_size': args.batch_size, 'sampler': sampler1}
-    test_kwargs = {'batch_size': args.test_batch_size, 'sampler': sampler2}
-    cuda_kwargs = {'num_workers': 2,
-                    'pin_memory': True,
-                    'shuffle': False}
-    train_kwargs.update(cuda_kwargs)
-    test_kwargs.update(cuda_kwargs)
+    train_kwargs = {'batch_size': args.batch_size}
+    test_kwargs = {'batch_size': args.test_batch_size}
 
     train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
-    auto_wrap_policy = functools.partial(
-        size_based_auto_wrap_policy, min_num_params=100
-    )
-    torch.cuda.set_device(rank)
 
-    init_start_event = torch.cuda.Event(enable_timing=True)
-    init_end_event = torch.cuda.Event(enable_timing=True)
+    init_start_event = torch.mps.Event(enable_timing=True)
+    init_end_event = torch.mps.Event(enable_timing=True)
 
-    model = Net().to(rank)
-
-    model = FSDP(model)
+    model = Net().to(device)
 
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     init_start_event.record()
     for epoch in range(1, args.epochs + 1):
-        train(args, model, rank, world_size, train_loader, optimizer, epoch, sampler=sampler1)
-        test(model, rank, world_size, test_loader)
+        train(args, model, train_loader, optimizer, epoch)
+        test(model, test_loader)
         scheduler.step()
 
     init_end_event.record()
@@ -130,7 +89,6 @@ def fsdp_main(rank, world_size, args):
         print(f"{model}")
 
     if args.save_model:
-        dist.barrier()
         states = model.state_dict()
         if rank == 0:
             torch.save(states, "mnist_cnn.pt")
@@ -159,8 +117,5 @@ if __name__ == '__main__':
 
     torch.manual_seed(args.seed)
 
-    WORLD_SIZE = torch.cuda.device_count()
-    mp.spawn(fsdp_main,
-        args=(WORLD_SIZE, args),
-        nprocs=WORLD_SIZE,
-        join=True)
+    print(f"mps device count: {torch.mps.device_count()}")
+    run(args)
