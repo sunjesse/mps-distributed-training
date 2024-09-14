050e56202690a89e5a1716963c07f89964db8135
sunjesse <jessera@outlook.com>
2024-09-14 19:03:48 -0400

    re-added distributed

--- train_mps.py
+++ train_mps.py
@@ -9,64 +9,83 @@ from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
 import torch.distributed as dist
-import torch.multiprocessing as mp
-from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data.distributed import DistributedSampler
 
 from model.net import Net
 
 _device = "mps" if torch.backends.mps.is_available() else "cpu"
 device = torch.device(_device)
 
-def train(args, model, train_loader, optimizer, epoch, sampler=None):
+def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12345'
+
+    dist.init_process_group("gloo", rank=int(rank), world_size=int(world_size))
+
+def cleanup():
+    dist.destroy_process_group()
+
+def train(args, rank, model, train_loader, optimizer, epoch, sampler=None):
     model.train()
     ddp_loss = torch.zeros(2)
     if sampler:
         sampler.set_epoch(epoch)
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+    for batch_idx, (x, y) in enumerate(train_loader):
+        x, y = x.to(device), y.to(device)
         optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target, reduction='sum')
+        logits = model(x)
+        loss = F.nll_loss(logits, y, reduction='sum')
         loss.backward()
         optimizer.step()
         ddp_loss[0] += loss.item()
-        ddp_loss[1] += len(data)
+        ddp_loss[1] += len(x)
 
-    print('Train Epoch: {} \tLoss: {:.6f}'.format(epoch, ddp_loss[0] / ddp_loss[1]))
+    dist.all_reduce(ddp_loss, op=dist.ReduceOp.SUM)
+    if rank == 0:
+        print('Train Epoch: {} \tLoss: {:.6f}'.format(epoch, ddp_loss[0] / ddp_loss[1]))
 
-def test(model, test_loader):
+def test(model, rank, test_loader):
     model.eval()
     correct = 0
     ddp_loss = torch.zeros(3)
     with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            ddp_loss[0] += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            ddp_loss[1] += pred.eq(target.view_as(pred)).sum().item()
-            ddp_loss[2] += len(data)
+        for x, y in test_loader:
+            x, y = x.to(device), y.to(device)
+            logits = model(x)
+            ddp_loss[0] += F.nll_loss(logits, y, reduction='sum').item()
+            pred = logits.argmax(dim=1, keepdim=True) 
+            ddp_loss[1] += pred.eq(y.view_as(pred)).sum().item()
+            ddp_loss[2] += len(y)
 
-    test_loss = ddp_loss[0] / ddp_loss[2]
-    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-        test_loss, int(ddp_loss[1]), int(ddp_loss[2]),
-        100. * ddp_loss[1] / ddp_loss[2]))
+    dist.all_reduce(ddp_loss, op=dist.ReduceOp.SUM)
+    if rank == 0:
+        test_loss = ddp_loss[0] / ddp_loss[2]
+        print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+            test_loss, int(ddp_loss[1]), int(ddp_loss[2]),
+            100. * ddp_loss[1] / ddp_loss[2]))
 
-def run(args): 
+def run(args, rank, world_size): 
+    setup(rank, world_size)
+
     transform=transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))])
 
-    dataset1 = datasets.MNIST('data/', train=True, download=True,
+    trainset = datasets.MNIST('data/', train=True, download=True,
                         transform=transform)
-    dataset2 = datasets.MNIST('data/', train=False,
+    testset = datasets.MNIST('data/', train=False,
                         transform=transform)
 
-    train_kwargs = {'batch_size': args.batch_size}
-    test_kwargs = {'batch_size': args.test_batch_size}
+    sampler_train = DistributedSampler(trainset, rank=rank, num_replicas=world_size, shuffle=True)
+    sampler_test = DistributedSampler(testset, rank=rank, num_replicas=world_size)
 
-    train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
-    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+    train_kwargs = {'batch_size': args.batch_size,
+                    'sampler': sampler_train}
+    test_kwargs = {'batch_size': args.test_batch_size,
+                   'sampler': sampler_test}
+
+    train_loader = torch.utils.data.DataLoader(trainset, **train_kwargs)
+    test_loader = torch.utils.data.DataLoader(testset, **test_kwargs)
 
     init_start_event = torch.mps.Event(enable_timing=True)
     init_end_event = torch.mps.Event(enable_timing=True)
@@ -77,17 +96,20 @@ def run(args):
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     init_start_event.record()
+
     for epoch in range(1, args.epochs + 1):
-        train(args, model, train_loader, optimizer, epoch)
-        test(model, test_loader)
+        train(args, rank, model, train_loader, optimizer, epoch, sampler=sampler_train)
+        test(model, rank, test_loader)
         scheduler.step()
 
     init_end_event.record()
 
-    print(f"Event elapsed time: {init_start_event.elapsed_time(init_end_event) / 1000}sec")
-    print(f"{model}")
+    if rank == 0:
+        print(f"Event elapsed time: {init_start_event.elapsed_time(init_end_event) / 1000}sec")
+        print(f"{model}")
 
     if args.save_model:
+        dist.barrier()
         states = model.state_dict()
         torch.save(states, "mnist_cnn.pt")
 
@@ -105,8 +127,6 @@ if __name__ == '__main__':
                         help='learning rate (default: 1.0)')
     parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
                         help='Learning rate step gamma (default: 0.7)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
     parser.add_argument('--save-model', action='store_true', default=False,
@@ -116,4 +136,6 @@ if __name__ == '__main__':
     torch.manual_seed(args.seed)
 
     print(f"mps device count: {torch.mps.device_count()}")
-    run(args)
+    RANK = int(os.environ["RANK"])
+    WORLD_SIZE = int(os.environ["WORLD_SIZE"])
+    run(args, rank=RANK, world_size=WORLD_SIZE)
